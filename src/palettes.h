ca7aca6c9049d3300343cc8e81cda6dcb55369ff
Gabe Conradi <gabe.conradi@gmail.com>
2018-08-07 12:19:43 -0400

    add a ton more palettes

--- src/palettes.h
+++ src/palettes.h
@@ -186,22 +186,157 @@ extern const NSFastLED::TProgmemRGBGradientPalette_byte Lucy_in_the_Sky_gp [] {
   204, 255,136, 15,
   255, 255,136, 15};
 
+
+// Gradient palette "PuRd_08_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/cb/seq/tn/PuRd_08.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 64 bytes of program space.
+
+extern const NSFastLED::TProgmemRGBGradientPalette_byte PuRd_08_gp [] {
+    0, 234,231,240,
+   31, 234,231,240,
+   31, 197,193,216,
+   63, 197,193,216,
+   63, 157,125,172,
+   95, 157,125,172,
+   95, 137, 77,137,
+  127, 137, 77,137,
+  127, 179, 33,100,
+  159, 179, 33,100,
+  159, 197,  4, 54,
+  191, 197,  4, 54,
+  191, 146,  1, 16,
+  223, 146,  1, 16,
+  223,  58,  0,  7,
+  255,  58,  0,  7};
+
+// Gradient palette "Oranges_06_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/cb/seq/tn/Oranges_06.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 48 bytes of program space.
+
+extern const NSFastLED::TProgmemRGBGradientPalette_byte Oranges_06_gp [] {
+    0, 252,217,180,
+   42, 252,217,180,
+   42, 249,162, 82,
+   84, 249,162, 82,
+   84, 249,109, 29,
+  127, 249,109, 29,
+  127, 249, 69,  6,
+  170, 249, 69,  6,
+  170, 194, 22,  1,
+  212, 194, 22,  1,
+  212,  83,  8,  1,
+  255,  83,  8,  1};
+
+// Gradient palette "bhw4_098_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw4/tn/bhw4_098.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 32 bytes of program space.
+
+extern const NSFastLED::TProgmemRGBGradientPalette_byte bhw4_098_gp [] {
+    0, 128, 33, 52,
+   35, 255, 17, 47,
+   58, 222,  2, 51,
+   99, 144, 56, 78,
+  124, 188,115,137,
+  178, 255, 16, 52,
+  219, 199,  1,  4,
+  255, 106,  1,  2};
+
+  // Gradient palette "bhw4_058_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw4/tn/bhw4_058.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 28 bytes of program space.
+
+extern const NSFastLED::TProgmemRGBGradientPalette_byte bhw4_058_gp [] {
+    0,  14,  2,  8,
+   51,  16,  5, 26,
+   96, 110, 17, 91,
+  127, 255, 35,133,
+  168, 132, 16, 95,
+  204,  83, 18, 80,
+  255,  14,  3, 19};
+
+// Gradient palette "bhw4_040_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw4/tn/bhw4_040.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 32 bytes of program space.
+
+extern const NSFastLED::TProgmemRGBGradientPalette_byte bhw4_040_gp [] {
+    0,  26,  7, 69,
+   73, 110, 36,240,
+  109, 249, 69,245,
+  142, 255,182,255,
+  173, 120,124,245,
+  204,  98, 59,207,
+  249,  30,  9,103,
+  255,  30,  9,103};
+
+// Gradient palette "bhw4_026_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw4/tn/bhw4_026.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 44 bytes of program space.
+
+extern const NSFastLED::TProgmemRGBGradientPalette_byte bhw4_026_gp [] {
+    0, 194,144,145,
+   17, 234, 32, 37,
+   43, 165,118,168,
+   73, 190,  7,  4,
+   96, 217, 33, 13,
+  119, 244, 54,  3,
+  137, 229,207,  2,
+  175, 197, 41, 61,
+  193, 165,118,168,
+  211, 234, 32, 37,
+  255, 227,125,147};
+
+// Gradient palette "bhw4_035_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw4/tn/bhw4_035.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 20 bytes of program space.
+
+extern const NSFastLED::TProgmemRGBGradientPalette_byte bhw4_035_gp [] {
+    0, 239, 22, 19,
+   43, 255, 92, 73,
+  112, 255,175, 62,
+  201, 118, 22, 47,
+  255,  21,  1, 21};
+
+// Gradient palette "bhw4_018_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw4/tn/bhw4_018.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 28 bytes of program space.
+
+extern const NSFastLED::TProgmemRGBGradientPalette_byte bhw4_018_gp [] {
+    0,  32,  1, 26,
+   45,  49,  1, 11,
+   76, 121,  2,  8,
+  104, 194,  7,  3,
+  127, 222, 36,  1,
+  160, 227,107,  1,
+  255,  92, 16,  2};
+
 // for effects that are palette based
 NSFastLED::CRGBPalette16 palettes[] = {
   Disorient_gp,
   Lucy_in_the_Sky_gp,
-  Bad_Kitten_gp,
   es_pinksplash_08_gp,
-  BlacK_Red_Magenta_Yellow_gp,
   es_pinksplash_07_gp,
-  Sunset_Real_gp,
   rgi_15_gp,
   bhw1_01_gp,
   bhw1_04_gp,
   bhw3_50_gp,
   bhw3_52_gp,
   bhw3_02_gp,
-  NSFastLED::LavaColors_p,
+  bhw4_018_gp,
+  bhw4_035_gp,
+  bhw4_026_gp,
+  bhw4_040_gp,
+  bhw4_058_gp,
+  bhw4_098_gp,
+  PuRd_08_gp,
+  Oranges_06_gp,
 };
 #define PALETTES_COUNT (sizeof(palettes)/sizeof(*palettes))
 
