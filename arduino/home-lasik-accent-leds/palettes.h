930aef1945d27acd41768096f01c65b933d362a9
Gabe Conradi <gabe.conradi@gmail.com>
2020-06-22 17:46:18 -0400

    adding home-lasik accent LEDs for trinket pro that does not crash

---
+++ arduino/home-lasik-accent-leds/palettes.h
@@ -0,0 +1,208 @@
+
+
+// Gradient palette "bhw1_01_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw1/tn/bhw1_01.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 12 bytes of program space.
+DEFINE_GRADIENT_PALETTE(bhw1_01_gp ) {
+    0, 227,101,  3,
+  117, 194, 18, 19,
+  255,  92,  8,192};
+
+// Gradient palette "bhw3_02_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw3/tn/bhw3_02.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 24 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( bhw3_02_gp ) {
+    0, 121,  1,  1,
+   63, 255, 57,  1,
+  112, 255, 79, 25,
+  145, 255, 79, 25,
+  188, 244,146,  3,
+  255, 115, 14,  1};
+
+
+
+
+// Gradient palette "es_pinksplash_07_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/es/pink_splash/tn/es_pinksplash_07.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 28 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( es_pinksplash_07_gp ) {
+    0, 229,  1,  1,
+   61, 242,  4, 63,
+  101, 255, 12,255,
+  127, 249, 81,252,
+  153, 255, 11,235,
+  193, 244,  5, 68,
+  255, 232,  1,  5};
+
+// Gradient palette "Lucy_in_the_Sky_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/colo/vredeling/tn/Lucy_in_the_Sky.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 40 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( Lucy_in_the_Sky_gp ) {
+    0, 175,  1, 44,
+   51, 175,  1, 44,
+   51, 194, 14, 35,
+  102, 194, 14, 35,
+  102, 213, 40, 27,
+  153, 213, 40, 27,
+  153, 234, 80, 21,
+  204, 234, 80, 21,
+  204, 255,136, 15,
+  255, 255,136, 15};
+
+
+// Gradient palette "bhw2_50_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw2/tn/bhw2_50.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 20 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( bhw2_50_gp ) {
+    0,   8,  2, 23,
+   84,  47,  7,102,
+  138,  16, 46,147,
+  173,   2,127,203,
+  255,   1,  7, 11};
+
+// Gradient palette "bhw2_48_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw2/tn/bhw2_48.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 28 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( bhw2_48_gp ) {
+    0,   0,  0,  8,
+   51,  20, 40,147,
+   61,  15,117,115,
+  109,  26,  3,186,
+  163,  78, 43,205,
+  211, 199, 17, 84,
+  255,  27,  1, 10};
+
+// Gradient palette "bhw4_040_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw4/tn/bhw4_040.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 32 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( bhw4_040_gp ) {
+    0,  26,  7, 69,
+   73, 110, 36,240,
+  109, 249, 69,245,
+  142, 255,182,255,
+  173, 120,124,245,
+  204,  98, 59,207,
+  249,  30,  9,103,
+  255,  30,  9,103};
+
+
+// Gradient palette "bhw4_035_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw4/tn/bhw4_035.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 20 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( bhw4_035_gp ) {
+    0, 239, 22, 19,
+   43, 255, 92, 73,
+  112, 255,175, 62,
+  201, 118, 22, 47,
+  255,  21,  1, 21};
+
+// Gradient palette "bhw4_018_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw4/tn/bhw4_018.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 28 bytes of program space.
+
+
+DEFINE_GRADIENT_PALETTE( bhw4_018_gp ) {
+    0,  32,  1, 26,
+   45,  49,  1, 11,
+   76, 121,  2,  8,
+  104, 194,  7,  3,
+  127, 222, 36,  1,
+  160, 227,107,  1,
+  255,  92, 16,  2};
+
+// Gradient palette "bhw2_sherbet2_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw2/tn/bhw2_sherbet2.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 32 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( bhw2_sherbet2_gp ) {
+    0, 217,  1,  1,
+   35, 249, 43, 19,
+   71, 247,125,172,
+  109, 206,  2, 32,
+  163, 210, 23,  9,
+  211, 255,255,255,
+  232, 252,199, 88,
+  255, 206,115, 52};
+
+// Gradient palette "bhw4_026_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw4/tn/bhw4_026.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 44 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( bhw4_026_gp ) {
+    0, 194,144,145,
+   17, 234, 32, 37,
+   43, 165,118,168,
+   73, 190,  7,  4,
+   96, 217, 33, 13,
+  119, 244, 54,  3,
+  137, 229,207,  2,
+  175, 197, 41, 61,
+  193, 165,118,168,
+  211, 234, 32, 37,
+  255, 227,125,147};
+
+  // Gradient palette "bhw4_098_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw4/tn/bhw4_098.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 32 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( bhw4_098_gp ) {
+    0, 128, 33, 52,
+   35, 255, 17, 47,
+   58, 222,  2, 51,
+   99, 144, 56, 78,
+  124, 188,115,137,
+  178, 255, 16, 52,
+  219, 199,  1,  4,
+  255, 106,  1,  2};
+
+    // Gradient palette "bhw4_058_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw4/tn/bhw4_058.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 28 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( bhw4_058_gp ) {
+    0,  14,  2,  8,
+   51,  16,  5, 26,
+   96, 110, 17, 91,
+  127, 255, 35,133,
+  168, 132, 16, 95,
+  204,  83, 18, 80,
+  255,  14,  3, 19};
+
+// for effects that are palette based
+CRGBPalette16 palettes[] = {
+  bhw4_018_gp, // good. pink orange dark
+  // bhw4_035_gp, // BAD white and some blue.
+  bhw4_026_gp, // good. white orange pink
+  // bhw4_040_gp, // BAD blue, no go
+  bhw4_098_gp, // good pink, white
+  bhw4_058_gp, // good purple blue
+  bhw2_sherbet2_gp, // good, white orange red
+  //Lucy_in_the_Sky_gp,
+  bhw1_01_gp,
+  bhw3_02_gp,
+  bhw2_48_gp,
+  bhw2_50_gp,
+};
+#define PALETTES_COUNT (sizeof(palettes)/sizeof(*palettes))
+
+
